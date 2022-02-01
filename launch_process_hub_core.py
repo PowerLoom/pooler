044ce99cda079af0e865f0f82165e7fc1ceecaa5
anomit ghosh <anomit.ghosh@gmail.com>
2022-02-01 17:26:06 +0530

    fix: update processhub core launcher namespacing for uniswap product

--- launch_process_hub_core.py
+++ launch_process_hub_core.py
@@ -1,6 +1,5 @@
 from init_rabbitmq import init_exchanges_queues
 from process_hub_core import ProcessHubCore
-from proto_system_logging import config_logger_with_namespace
 import logging
 import logging.handlers
 import signal
@@ -19,25 +18,25 @@ def main():
     signal.signal(signal.SIGTERM, sigterm_handler)
     # logging.config.dictConfig(config_logger_with_namespace('PowerLoom|ProcessHub|Core|Launcher'))
     # logging.config.dictConfig(config_logger_with_namespace(namespace=None))
-    logger = logging.getLogger('PowerLoom|ProcessHub|Core|Launcher')
+    logger = logging.getLogger('PowerLoom|UniswapPoolerProcessHub|Core|Launcher')
     logger.setLevel(logging.DEBUG)
     logger.handlers = [logging.handlers.SocketHandler(host='localhost', port=logging.handlers.DEFAULT_TCP_LOGGING_PORT)]
     init_exchanges_queues()
-    core = ProcessHubCore(name='PowerLoomProcessHubCore')
+    core = ProcessHubCore(name='PowerLoom|UniswapPoolerProcessHub|Core')
     core.start()
-    logger.debug('Launched ProcessHubCore with PID %s', core.pid)
+    logger.debug('Launched PowerLoom|UniswapPoolerProcessHub|Core with PID %s', core.pid)
     try:
         core.join()
     except KeyboardInterrupt:
-        logger.debug('ProcessHubCore Launcher received SIGINT. Will attempt to join with ProcessHubCore process...')
+        logger.debug('PowerLoom|UniswapPoolerProcessHub|Core Launcher received SIGINT. Will attempt to join with ProcessHubCore process...')
     except SIGTERMExit:
-        logger.debug('ProcessHubCore Launcher received SIGTERM. Will attempt to join with ProcessHubCore process...')
+        logger.debug('PowerLoom|UniswapPoolerProcessHub|Core Launcher received SIGTERM. Will attempt to join with ProcessHubCore process...')
     finally:
         try:
             core.join()
         except:
             pass
-        logger.debug('ProcessHubCore Launcher exiting. ProcessHubCore exited and joined with parent process...')
+        logger.debug('PowerLoom|UniswapPoolerProcessHub|Core Launcher exiting. ProcessHubCore exited and joined with parent process...')
 
 
 if __name__ == '__main__':
