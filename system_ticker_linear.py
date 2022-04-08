0d1cc2a0457e796330d21ed07ee7eac86818195b
prem chaitanya <chaitanyaprem@gmail.com>
2022-04-08 11:14:14 +0530

    fix: Fixed chunking logic to generate 2 epochs when 11 blocks are identified. It was missing generating second epoch.

--- system_ticker_linear.py
+++ system_ticker_linear.py
@@ -21,7 +21,7 @@ def chunks(start_idx, stop_idx, n):
     for i in range(start_idx, stop_idx+1, n):
         # Create an index range for l of n items:
         begin_idx = i  # if run_idx == 0 else i+1
-        if begin_idx == stop_idx:
+        if begin_idx == stop_idx+1:
             return
         end_idx = i + n - 1 if i + n - 1 <= stop_idx else stop_idx
         run_idx += 1
