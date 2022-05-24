ef4650ea8786987434323c6cbbc5bd9ec9eb3db3
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2022-05-24 17:33:12 +0530

    fix: use new redis connection

--- register_pair_projects_for_indexing.py
+++ register_pair_projects_for_indexing.py
@@ -1,12 +1,11 @@
 from dynaconf import settings
-from redis_conn import provide_redis_conn_insta
+from redis_conn import REDIS_CONN_CONF
 import os
 import json
-import redis
+from redis import Redis
 
 
-@provide_redis_conn_insta
-def main(redis_conn: redis.Redis=None):
+def main():
     if not os.path.exists('static/cached_pair_addresses.json'):
         return
     f = open('static/cached_pair_addresses.json', 'r')
@@ -14,13 +13,14 @@ def main(redis_conn: redis.Redis=None):
 
     if len(pairs) <= 0:
         return
+    r = Redis(**REDIS_CONN_CONF)
     streams = ['pair_total_reserves', 'trade_volume']
     project_ids = dict()
     for each_pair in pairs:
         addr = each_pair.lower()
         for stream in streams:
             project_ids.update({f'uniswap_pairContract_{stream}_{addr}_{settings.NAMESPACE}': json.dumps({'series': ['24h', '7d']})})
-    redis_conn.hset('cache:indexesRequested', mapping=project_ids)
+    r.hset('cache:indexesRequested', mapping=project_ids)
 
 
 if __name__ == '__main__':
