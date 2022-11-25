c317628b3837615001b7d9ec975ece4187b08c8d
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2022-11-25 16:15:02 +0530

    fix test scripts and warnings in the same

--- tests/test_rmq_basic_publish.py
+++ tests/test_rmq_basic_publish.py
@@ -1,20 +1,19 @@
-from init_rabbitmq import create_rabbitmq_conn
-from rabbitmq_helpers import RabbitmqThreadedSelectLoopInteractor
-from dynaconf import settings
-import pika
 import queue
 import threading
-import time
+import pika
+from dynaconf import settings
 
+from init_rabbitmq import create_rabbitmq_conn
+from rabbitmq_helpers import RabbitmqThreadedSelectLoopInteractor
 
-def interactor_wrapper_obj(q: queue.Queue):
-    s = RabbitmqThreadedSelectLoopInteractor(publish_queue=q)
-    s.run()
+def interactor_wrapper_obj(rmq_q: queue.Queue):
+    rmq_interactor = RabbitmqThreadedSelectLoopInteractor(publish_queue=rmq_q)
+    rmq_interactor.run()
 
 
 if __name__ == '__main__':
     q = queue.Queue()
-    cmd = '{"command": "start", "pid": null, "proc_str_id": "EpochCallbackManager", "init_kwargs": {}}'
+    CMD = '{"command": "start", "pid": null, "proc_str_id": "EpochCallbackManager", "init_kwargs": {}}'
     exchange = f'{settings.RABBITMQ.SETUP.CORE.EXCHANGE}:{settings.NAMESPACE}'
     routing_key = f'processhub-commands:{settings.NAMESPACE}'
     try:
@@ -25,10 +24,10 @@ if __name__ == '__main__':
         if i == 1:
             c = create_rabbitmq_conn()
             ch = c.channel()
-            r = ch.basic_publish(
+            ch.basic_publish(
                 exchange=f'{settings.RABBITMQ.SETUP.CORE.EXCHANGE}:{settings.NAMESPACE}',
                 routing_key = f'processhub-commands:{settings.NAMESPACE}',
-                body=cmd.encode('utf-8'),
+                body=CMD.encode('utf-8'),
                 properties=pika.BasicProperties(
                     delivery_mode=2,
                     content_type='text/plain',
@@ -36,10 +35,10 @@ if __name__ == '__main__':
                 ),
                 mandatory=True
             )
-            print('Vanilla publish return value: ', r)
+            print('Published to rabbitmq')
         else:
             print('Trying to publish via select loop adapter...')
-            brodcast_msg = (cmd.encode('utf-8'), exchange, routing_key)
+            brodcast_msg = (CMD.encode('utf-8'), exchange, routing_key)
             q.put(brodcast_msg)
     except KeyboardInterrupt:
         t.join()
