fdd889d230b9df8602000842b68b7b0ddb78e8f9
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2022-11-25 16:15:02 +0530

    fix test scripts and warnings in the same

--- tests/test_liquidity.py
+++ tests/test_liquidity.py
@@ -1,49 +1,61 @@
-from web3 import Web3
-from dynaconf import settings
 import asyncio
-from functools import partial
 import json
-from uniswap_functions import get_liquidity_of_each_token_reserve_async
 import os
 import requests
+from redis import asyncio as aioredis
+
+from uniswap_functions import (get_pair_reserves,
+                                load_rate_limiter_scripts,
+                                provide_async_redis_conn_insta)
 
 def load_contracts():
     contracts = list()
     if os.path.exists('static/cached_pair_addresses.json'):
-        with open('static/cached_pair_addresses.json', 'r') as fp:
+        with open('static/cached_pair_addresses.json', 'r', encoding='utf-8') as fp:
             # the file contains an array of pair contract addresses
             contracts = json.load(fp)
             return contracts
 
-def fetch_liquidityUSD_rpc(pair_address):
-    loop = asyncio.get_event_loop()
-    rate_limit_lua_script_shas = dict()
-    data = loop.run_until_complete(
-        get_liquidity_of_each_token_reserve_async(loop, rate_limit_lua_script_shas, pair_address )
-    )
-    return data['token0USD']+data['token1USD']
+@provide_async_redis_conn_insta
+async def fetch_liquidityUSD_rpc(pair_address,block_num, redis_conn: aioredis.Redis = None):
+    rate_limiting_lua_scripts = await load_rate_limiter_scripts(redis_conn)
+    data = await get_pair_reserves(loop, rate_limiting_lua_scripts, pair_address, block_num,block_num, redis_conn=redis_conn)
+    block_pair_total_reserves= data.get(block_num)
+    return block_pair_total_reserves['token0USD']+block_pair_total_reserves['token1USD']
 
 
-def fetch_liquidityUSD_graph(pair_address):
+def fetch_liquidityUSD_graph(pair_address, block_num):
     uniswap_url = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2"
-    uniswap_payload = "{\"query\":\"{\\n  mints(orderBy: timestamp, orderDirection: desc, where:{\\n    pair: \\\""+str(pair_address)+"\\\",\\n    timestamp_lt: "+ str(current_time)+ "\\n  }) {\\n    id,\\n    timestamp,\\n    amountUSD,\\n    logIndex,\\n    feeTo,\\n    feeLiquidity,\\n    amount0,\\n    amount1\\n  },\\n  swaps(orderBy: timestamp, orderDirection: desc, where:{\\n    pair: \\\""+str(pair_contract)+"\\\",\\n    timestamp_lt: "+ str(current_time)+ "\\n  }) {\\n    id,\\n    timestamp,\\n    amountUSD,\\n    logIndex,\\n    amount0In,\\n    amount0Out,\\n    amount1Out,\\n    amount1In\\n  },\\n  burns(orderBy: timestamp, orderDirection: desc, where:{\\n    pair: \\\""+str(pair_contract)+"\\\",\\n    timestamp_lt: "+ str(current_time)+ "\\n  }) {\\n    id,\\n    timestamp,\\n    amountUSD,\\n    logIndex,\\n    amount0,\\n    amount1\\n  }\\n}\\n\",\"variables\":null}"
-    headers = {'Content-Type': 'text/plain'}
-    response = requests.request("POST", uniswap_url, headers=headers, data=uniswap_payload)
+    uniswap_payload = "{\"query\":\"{\\n pair(id: \\\"" + str(pair_address) + "\\\",block:{number:"+ str(block_num) + "}) {\\n reserveUSD \\n token0 { \\n     symbol \\n } \\n token1 { \\n      symbol \\n    }\\n  } \\n }\" }"
+    print(uniswap_payload)
+    headers = {'Content-Type': 'application/plain'}
+    response = requests.request("POST", uniswap_url, headers=headers, data=uniswap_payload, timeout=30)
     if response.status_code == 200:
-        timestamp_24h = current_time - 60 * 60 * 24
-        total_trades_in_usd = 0
         data = json.loads(response.text)
+        print("Response",data)
         data = data["data"]
-        for each_event in data:
-            for obj in data[each_event]:
-                if int(obj['timestamp']) >= int(timestamp_24h):
-                    debug_trade_logs["uniswap"][each_event].append(obj)
-                    total_trades_in_usd += int(float(obj['amountUSD']))
-        trade_volume_data["uniswap_24h_trade_volume"] = int(total_trades_in_usd)
+        return float(data['pair']['reserveUSD'])
     else:
-        print(f"Error fetching data from uniswap THE GRAPH")
-        trade_volume_data["uniswap_24h_trade_volume"] = None
+        print("Error fetching data from uniswap THE GRAPH %s",response)
+        return 0
 
+async def compare_liquidity():
+    #contracts = load_contracts()
+    total_liquidity_usd_graph = 0
+    total_liquidity_usd_rpc = 0
+    block_num = 16046250
 
+    contracts = list()
+    contracts.append("0xae461ca67b15dc8dc81ce7615e0320da1a9ab8d5")
+    for contract in contracts:
+        liquidity_usd_graph = fetch_liquidityUSD_graph(contract, block_num)
+        liquidity_usd_rpc = await fetch_liquidityUSD_rpc(contract, block_num)
+        print(f"Contract {contract}, liquidityUSD_graph is {liquidity_usd_graph} , liquidityUSD_rpc {liquidity_usd_rpc}, liquidityUSD difference {(liquidity_usd_rpc - liquidity_usd_graph)}")
+        total_liquidity_usd_graph += liquidity_usd_graph
+        total_liquidity_usd_rpc += liquidity_usd_rpc
 
-liq = fetch_liquidityUSD('0x70258aa9830c2c84d855df1d61e12c256f6448b4')
+    print(f"{len(contracts)} contracts compared, liquidityUSD_rpc_total is {total_liquidity_usd_rpc}, liquidityUSD_graph_total is {total_liquidity_usd_graph}")
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(compare_liquidity())
