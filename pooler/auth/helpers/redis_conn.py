4c4111c9842ec0e65e26b9c62ddc19a3f1a74db8
Akshay Dahiya <akshay@powerloom.io>
2023-06-22 17:48:20 +0530

    chore: switch to custom ConnectionPool for redis

--- pooler/auth/helpers/redis_conn.py
+++ pooler/auth/helpers/redis_conn.py
@@ -1,5 +1,6 @@
 import redis
 from redis import asyncio as aioredis
+from redis.asyncio.connection import ConnectionPool
 
 from pooler.auth.conf import auth_settings
 
@@ -13,13 +14,17 @@ def construct_redis_url():
     else:
         return f'redis://{auth_settings.redis.host}:{auth_settings.redis.port}/{auth_settings.redis.db}'
 
+# ref https://github.com/redis/redis-py/issues/936
+
 
 async def get_aioredis_pool(pool_size=200):
-    return await aioredis.from_url(
+    pool = ConnectionPool.from_url(
         url=construct_redis_url(),
         retry_on_error=[redis.exceptions.ReadOnlyError],
         max_connections=pool_size,
     )
+
+    return aioredis.Redis(connection_pool=pool)
 
 
 class RedisPoolCache:
