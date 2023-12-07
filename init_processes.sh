3890a13b74d253ddb33e94c4057eefa24dafbc7e
Akshay Dahiya <akshay@powerloom.io>
2023-12-07 22:11:15 +0530

    chore: disable everything except core api, update web3 to latest version

--- init_processes.sh
+++ init_processes.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-poetry run python -m snapshotter.snapshotter_id_ping
-ret_status=$?
+# poetry run python -m snapshotter.snapshotter_id_ping
+# ret_status=$?
 
-if [ $ret_status -ne 0 ]; then
-    echo "Snapshotter identity check failed on protocol smart contract"
-    exit 1
-fi
-echo 'starting processes...';
+# if [ $ret_status -ne 0 ]; then
+#     echo "Snapshotter identity check failed on protocol smart contract"
+#     exit 1
+# fi
+# echo 'starting processes...';
 pm2 start pm2.config.js
 
 # Waiting for other processes to start
 echo 'waiting for processes to start..';
 sleep 10
 
-poetry run python -m snapshotter.processhub_cmd start ProcessorDistributor
-sleep 3
+# poetry run python -m snapshotter.processhub_cmd start ProcessorDistributor
+# sleep 3
 
-poetry run python -m snapshotter.processhub_cmd start SystemEventDetector
+# poetry run python -m snapshotter.processhub_cmd start SystemEventDetector
 
-echo 'started all snapshotter scripts';
+# echo 'started all snapshotter scripts';
 
 pm2 logs --lines 1000
--- pm2.config.js
+++ pm2.config.js
@@ -7,29 +7,29 @@ const MIN_UPTIME = 60000;
 
 module.exports = {
   apps : [
-    {
-      name   : "process-hub-core",
-      script : `poetry run python -m snapshotter.launch_process_hub_core`,
-      max_restarts: MAX_RESTART,
-      min_uptime: MIN_UPTIME,
-      error_file: "/dev/null",
-      out_file: "/dev/null",
-      env: {
-        NODE_ENV: NODE_ENV,
-      }
-    },
-    {
-      name   : "core-api",
-      script : `poetry run python -m snapshotter.gunicorn_core_launcher`,
-      max_restarts: MAX_RESTART,
-      min_uptime: MIN_UPTIME,
-      error_file: "/dev/null",
-      out_file: "/dev/null",
-      env: {
-        NODE_ENV: NODE_ENV,
-        GUNICORN_WORKERS: 1,
-      }
-    },
+    // {
+    //   name   : "process-hub-core",
+    //   script : `poetry run python -m snapshotter.launch_process_hub_core`,
+    //   max_restarts: MAX_RESTART,
+    //   min_uptime: MIN_UPTIME,
+    //   error_file: "/dev/null",
+    //   out_file: "/dev/null",
+    //   env: {
+    //     NODE_ENV: NODE_ENV,
+    //   }
+    // },
+    // {
+    //   name   : "core-api",
+    //   script : `poetry run python -m snapshotter.gunicorn_core_launcher`,
+    //   max_restarts: MAX_RESTART,
+    //   min_uptime: MIN_UPTIME,
+    //   error_file: "/dev/null",
+    //   out_file: "/dev/null",
+    //   env: {
+    //     NODE_ENV: NODE_ENV,
+    //     GUNICORN_WORKERS: 1,
+    //   }
+    // },
     {
       name   : "auth-api",
       script : `poetry run python -m snapshotter.auth.gunicorn_auth_entry_launcher`,
