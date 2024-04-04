5a2a0901f02698f2e1badfb717a85073543f595a
anomit ghosh <anomit.ghosh@gmail.com>
2024-04-04 23:48:46 +0800

    chore: remove master snapshotter check for devnet nodes

--- snapshotter/snapshotter_id_ping.py
+++ snapshotter/snapshotter_id_ping.py
@@ -20,31 +20,12 @@ async def main():
     aioredis_pool = RedisPoolCache(pool_size=1000)
     await aioredis_pool.populate()
     redis_conn = aioredis_pool._aioredis_pool
-    anchor_rpc = RpcHelper(settings.anchor_chain_rpc)
-    protocol_abi = read_json_file(settings.protocol_state.abi)
-    protocol_state_contract = anchor_rpc.get_current_node()['web3_client'].eth.contract(
-        address=Web3.toChecksumAddress(
-            settings.protocol_state.address,
-        ),
-        abi=protocol_abi,
+    
+    await redis_conn.set(
+        active_status_key,
+        int(True),
     )
-    snapshotter_query = await anchor_rpc.web3_call(
-        [
-            protocol_state_contract.functions.slotSnapshotterMapping(settings.slot_id),
-        ],
-        redis_conn,
-    )
-    snapshotter_address = snapshotter_query[0]
-    if snapshotter_address != to_checksum_address(settings.instance_id):
-        print('Signer Account is not the one configured in slot, exiting!')
-        sys.exit(1)
-    else:
-        await redis_conn.set(
-            active_status_key,
-            int(True),
-        )
-        print('Signer Account is the one configured in slot, all good!')
-        sys.exit(0)
+    sys.exit(0)
 
 if __name__ == '__main__':
     asyncio.run(main())
