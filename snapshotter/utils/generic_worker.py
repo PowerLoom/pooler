0967d4c396a0bb908ac50591a8126a285308bbc4
Akshay Dahiya <akshay@powerloom.io>
2024-04-19 14:11:16 +0530

    chore: add safety check while resetting nonce

--- snapshotter/utils/generic_worker.py
+++ snapshotter/utils/generic_worker.py
@@ -1,8 +1,6 @@
 import asyncio
 import json
 import multiprocessing
-import sys
-import sha3
 import resource
 import time
 from functools import partial
@@ -10,22 +8,24 @@ from signal import SIGINT
 from signal import signal
 from signal import SIGQUIT
 from signal import SIGTERM
-from typing import Dict, Optional
+from typing import Dict
 from typing import Union
 from uuid import uuid4
+
+import aiorwlock
+import httpx
+import sha3
+import tenacity
+from aio_pika import IncomingMessage
+from aio_pika import Message
+from aio_pika.pool import Pool
+from coincurve import PrivateKey
 from eip712_structs import EIP712Struct
 from eip712_structs import make_domain
 from eip712_structs import String
 from eip712_structs import Uint
-from eth_utils.encoding import big_endian_to_int
-import aiorwlock
-import httpx
-import tenacity
-from coincurve import PrivateKey
-from aio_pika import IncomingMessage
-from aio_pika import Message
-from aio_pika.pool import Pool
 from eth_utils.crypto import keccak
+from eth_utils.encoding import big_endian_to_int
 from httpx import AsyncClient
 from httpx import AsyncHTTPTransport
 from httpx import Limits
@@ -34,10 +34,12 @@ from ipfs_client.dag import IPFSAsyncClientError
 from ipfs_client.main import AsyncIPFSClient
 from pydantic import BaseModel
 from redis import asyncio as aioredis
-from tenacity import retry, retry_if_exception_type
+from tenacity import retry
+from tenacity import retry_if_exception_type
 from tenacity import stop_after_attempt
 from tenacity import wait_random_exponential
 from web3 import Web3
+
 from snapshotter.settings.config import settings
 from snapshotter.utils.callback_helpers import get_rabbitmq_channel
 from snapshotter.utils.callback_helpers import get_rabbitmq_robust_connection_async
@@ -46,10 +48,12 @@ from snapshotter.utils.data_utils import get_source_chain_id
 from snapshotter.utils.default_logger import logger
 from snapshotter.utils.file_utils import read_json_file
 from snapshotter.utils.helper_functions import aiorwlock_aqcuire_release
-from snapshotter.utils.models.data_models import SignRequest, SnapshotSubmissionSignerState, SnapshotterIssue, TxnPayload
+from snapshotter.utils.models.data_models import SignRequest
+from snapshotter.utils.models.data_models import SnapshotterIssue
 from snapshotter.utils.models.data_models import SnapshotterReportState
 from snapshotter.utils.models.data_models import SnapshotterStates
 from snapshotter.utils.models.data_models import SnapshotterStateUpdate
+from snapshotter.utils.models.data_models import TxnPayload
 from snapshotter.utils.models.data_models import UnfinalizedSnapshot
 from snapshotter.utils.models.message_models import AggregateBase
 from snapshotter.utils.models.message_models import PayloadCommitMessage
@@ -61,7 +65,6 @@ from snapshotter.utils.redis.redis_keys import epoch_id_project_to_state_mapping
 from snapshotter.utils.redis.redis_keys import submitted_unfinalized_snapshot_cids
 from snapshotter.utils.rpc import RpcHelper
 from snapshotter.utils.transaction_utils import write_transaction
-from asyncio import Queue
 
 
 class Request(EIP712Struct):
@@ -85,36 +88,6 @@ def web3_storage_retry_state_callback(retry_state: tenacity.RetryCallState):
     if retry_state and retry_state.outcome.failed:
         logger.warning(
             f'Encountered web3 storage upload exception: {retry_state.outcome.exception()} | args: {retry_state.args}, kwargs:{retry_state.kwargs}',
-        )
-
-
-def submit_snapshot_retry_callback(retry_state: tenacity.RetryCallState):
-    if retry_state.attempt_number >= 3:
-        logger.error(
-            'Txn signing worker failed after 3 attempts | Txn payload: {} | Signer: {}', retry_state.kwargs['txn_payload'], retry_state.kwargs['signer_in_use'].address
-        )
-    else:
-        if retry_state.outcome.failed:
-            if 'nonce' in str(retry_state.outcome.exception()):
-                # reassigning the signer object to ensure nonce is reset
-                # basically retry_state.args[0] accesses the self object. 
-                # self._signer is the signer object
-                retry_state.kwargs['signer_in_use'] = retry_state.args[0]._signer  
-                logger.warning(
-                    'Tx signing worker attempt number {} result {} failed with nonce exception | Reset nonce and reassigned signer object: {} with nonce {} | Txn payload: {}',
-                    retry_state.attempt_number, retry_state.outcome, retry_state.kwargs["signer_in_use"].address, 
-                    retry_state.kwargs['signer_in_use'].nonce, retry_state.kwargs["txn_payload"]
-                )
-            else:
-                logger.warning(
-                    'Tx signing worker attempt number {} result {} failed with exception {} | Txn payload: {}', 
-                    retry_state.attempt_number, retry_state.outcome, retry_state.outcome.exception(), retry_state.kwargs["txn_payload"]
-                )
-        logger.warning(
-            'Tx signing worker {} attempt number {} result {} | Txn payload: {}', 
-            retry_state.kwargs['signer_in_use'].address, retry_state.attempt_number, retry_state.outcome,
-            retry_state.kwargs['txn_payload'],
-
         )
 
 
@@ -148,11 +121,10 @@ class GenericAsyncWorker(multiprocessing.Process):
     _chain_id: int
     _epoch_size: int
     _source_chain_block_time: int
-    _signer: SnapshotSubmissionSignerState
-    _signer_private_key: str
+    _signer_pkey: str
     _signer_nonce: int
     _signer_address: str
-    
+
     def __init__(self, name, signer_idx, **kwargs):
         """
         Initializes a GenericAsyncWorker instance.
@@ -171,7 +143,6 @@ class GenericAsyncWorker(multiprocessing.Process):
         # this acts as the index of the signer to use from the list in settings for self submission
         self._signer_index = signer_idx
         self._rate_limiting_lua_scripts = None
-        self.pending_nonces = Queue()
 
         self.protocol_state_contract_address = Web3.toChecksumAddress(settings.protocol_state.address)
         self._commit_payload_exchange = (
@@ -271,7 +242,10 @@ class GenericAsyncWorker(multiprocessing.Process):
         s = big_endian_to_int(signature[32:64])
 
         final_sig = r.to_bytes(32, 'big') + s.to_bytes(32, 'big') + v.to_bytes(1, 'big')
-        request_ = {'slotId': 0, 'deadline': deadline, 'snapshotCid': snapshot_cid, 'epochId': epoch_id, 'projectId': project_id}
+        request_ = {
+            'slotId': 0, 'deadline': deadline, 'snapshotCid': snapshot_cid,
+            'epochId': epoch_id, 'projectId': project_id,
+        }
         return request_, final_sig
 
     async def _commit_payload(
@@ -389,7 +363,7 @@ class GenericAsyncWorker(multiprocessing.Process):
             else:
                 request_, sig = self.generate_signature(snapshot_cid, epoch.epochId, project_id)
                 await self.submit_snapshot(
-                    TxnPayload(
+                    txn_payload=TxnPayload(
                         slotId=request_['slotId'],
                         snapshotCid=snapshot_cid,
                         epochId=epoch.epochId,
@@ -403,7 +377,7 @@ class GenericAsyncWorker(multiprocessing.Process):
                         ),
                         signature='0x' + str(sig.hex()),
                         contractAddress=settings.protocol_state.address,
-                    )
+                    ),
                 )
 
         # upload to web3 storage
@@ -538,33 +512,57 @@ class GenericAsyncWorker(multiprocessing.Process):
             )
 
     @aiorwlock_aqcuire_release
+    async def _increment_nonce(self):
+        self._signer_nonce += 1
+        self._logger.info(
+            'Using signer {} for submission task. Incremented nonce {}',
+            self._signer_address, self._signer_nonce,
+        )
+
+    @aiorwlock_aqcuire_release
+    async def _reset_nonce(self, value: int = 0):
+        if value > 0:
+            self._signer_nonce = value
+            self._logger.info(
+                'Using signer {} for submission task. Reset nonce to {}',
+                self._signer_address, self._signer_nonce,
+            )
+        else:
+            correct_nonce = await self._w3.eth.get_transaction_count(
+                self._signer_address,
+            )
+            if correct_nonce and type(correct_nonce) is int:
+                self._signer_nonce = correct_nonce
+                self._logger.info(
+                    'Using signer {} for submission task. Reset nonce to {}',
+                    self._signer_address, self._signer_nonce,
+                )
+            else:
+                self._logger.error(
+                    'Using signer {} for submission task. Could not reset nonce',
+                    self._signer_address,
+                )
+
     @retry(
         reraise=True,
         retry=retry_if_exception_type(Exception),
-        wait=wait_random_exponential(multiplier=1, max=10),
+        wait=wait_random_exponential(multiplier=1, max=2),
         stop=stop_after_attempt(3),
-        after=submit_snapshot_retry_callback
     )
-    async def submit_snapshot(self, txn_payload: TxnPayload, signer_in_use: Optional[SnapshotSubmissionSignerState] = None):
+    async def submit_snapshot(self, txn_payload: TxnPayload):
         """
         Submit Snapshot
         """
-        if signer_in_use is None:
-            self._logger.warning('No signer passed to submit_snapshot, quitting')
-            return None
-        if self.pending_nonces.empty():
-            _nonce = signer_in_use.nonce
-        else:
-            try:
-                _nonce = self.pending_nonces.get_nowait()
-            except asyncio.QueueEmpty:
-                _nonce = signer_in_use.nonce
+        _nonce = self._signer_nonce
+        await self._increment_nonce()
+        self._logger.trace(f'nonce: {_nonce}')
+
         try:
             tx_hash = await write_transaction(
                 self._w3,
                 self._chain_id,
-                signer_in_use.address,
-                signer_in_use.private_key,
+                self._signer_address,
+                self._signer_pkey,
                 self._protocol_state_contract,
                 'submitSnapshot',
                 _nonce,
@@ -583,25 +581,31 @@ class GenericAsyncWorker(multiprocessing.Process):
             self._logger.info(
                 f'submitted transaction with tx_hash: {tx_hash}',
             )
+            await self._w3.eth.wait_for_transaction_receipt(tx_hash, timeout=20)
 
         except Exception as e:
-            self._logger.opt(exception=True).error(f'Exception: {e}')
-
-            if 'nonce' in str(e):
-                # sleep for 10 seconds and reset nonce
-                await asyncio.sleep(10)
-                self._signer.nonce = await self._w3.eth.get_transaction_count(
-                    signer_in_use.address,
-                )
+            if 'nonce too low' in str(e):
+                error = eval(str(e))
+                message = error['message']
+                next_nonce = int(message.split('next nonce ')[1].split(',')[0])
                 self._logger.info(
-                    f'nonce for {self._signer.address} reset to: {self._signer.nonce}',
+                    'Nonce too low error. Next nonce: {}', next_nonce,
                 )
+                await self._reset_nonce(next_nonce)
+                # reset queue
                 raise Exception('nonce error, reset nonce')
             else:
-                raise Exception('other error, still retrying')
+                self._logger.info(
+                    'Error submitting snapshot. Retrying...',
+                )
+                # sleep for two seconds before updating nonce
+                time.sleep(2)
+                await self._reset_nonce()
+
+                raise e
         else:
             return tx_hash
-     
+
     async def _on_rabbitmq_message(self, message: IncomingMessage):
         """
         Callback function that is called when a message is received from RabbitMQ.
@@ -628,7 +632,6 @@ class GenericAsyncWorker(multiprocessing.Process):
         await self._anchor_rpc_helper.init(redis_conn=self._redis_conn)
         await self._anchor_rpc_helper._load_async_web3_providers()
         self._logger.info('Anchor chain RPC helper nodes: {}', self._anchor_rpc_helper._nodes)
-        # sys.exit(1)
         self._protocol_state_contract = self._anchor_rpc_helper.get_current_node()['web3_client'].eth.contract(
             address=Web3.toChecksumAddress(
                 self.protocol_state_contract_address,
@@ -643,14 +646,9 @@ class GenericAsyncWorker(multiprocessing.Process):
         # web3 v5 camel case helpers
         self._signer_address = Web3.toChecksumAddress(settings.snapshot_submissions.signers[self._signer_index].address)
         self._signer_nonce = await self._w3.eth.get_transaction_count(self._signer_address)
-        self._signer_private_key = settings.snapshot_submissions.signers[self._signer_index].private_key
-        self._signer = SnapshotSubmissionSignerState(
-            address=self._signer_address,
-            private_key=self._signer_private_key,
-            nonce=self._signer_nonce,
-            nonce_lock=aiorwlock.RWLock(fast=True),
-        )
-        self._logger.debug('Picked signer {} at index {} and nonce {} for self submission', self._signer_address, self._signer_index, self._signer_nonce)
+        self._signer_pkey = settings.snapshot_submissions.signers[self._signer_index].private_key
+        self._rwlock = aiorwlock.RWLock(fast=True)
+
         self._chain_id = await self._w3.eth.chain_id
         self._logger.debug('Set anchor chain ID to {}', self._chain_id)
         self._domain_separator = make_domain(
--- snapshotter/utils/helper_functions.py
+++ snapshotter/utils/helper_functions.py
@@ -1,5 +1,4 @@
 import asyncio
-import random
 import sys
 from functools import wraps
 
@@ -193,52 +192,30 @@ def aiorwlock_aqcuire_release(fn):
     """
     @wraps(fn)
     async def wrapper(self, *args, **kwargs):
-        self._logger.info('Using signer {} for submission task. Acquiring lock', self._signer.address)
-        await self._signer.nonce_lock.writer_lock.acquire()
-        kwargs.update(signer_in_use=self._signer)
-        self._logger.info('Using signer {} for submission task. Acquired lock with signer filled in kwargs', self._signer.address)
+        self._logger.info(
+            'Using signer {} for submission task. Acquiring lock', self._signer_address,
+        )
+        await self._rwlock.writer_lock.acquire()
+        self._logger.info(
+            'Using signer {} for submission task. Acquired lock', self._signer_address,
+        )
         # self._logger.debug('Wrapping fn: {}', fn.__name__)
         try:
-            tx_hash = await fn(self, *args, **kwargs)  # including the retry calls
-            self._signer.nonce += 1
-            self._logger.info('Using signer {} for submission task. Incremented nonce {}', self._signer.address, self._signer.nonce)
-            try:
-                self._signer.nonce_lock.writer_lock.release()
-            except Exception as e:
-                logger.error('Error releasing rwlock: {}. But moving on regardless... | Context: '
-                             'Using signer {} for submission task. Acquiring lock', e, self._signer.address)
+            # including the retry calls
+            await fn(self, *args, **kwargs)
+
         except Exception as e:
-            # this is ultimately reraised by tenacity once the retries are exhausted
+            self._logger.opt(exception=True).error(
+                'Error in using signer {} for submission task: {}', self._signer_address, e,
+            )
             # nothing to do here
             pass
-        else:
-            if not tx_hash:
-                self._logger.info('tx_hash is None for submission task')
-                self.pending_nonces.put(self._signer.nonce)
-            if tx_hash is not None:
-                try:
-                    receipt = await self._w3.eth.wait_for_transaction_receipt(tx_hash)
-                    if receipt['status'] == 0:
-                        self._logger.info(
-                            'tx_hash: {} failed to gather success receipt after 120 seconds, receipt: {} | '
-                            'Context: Using signer {} for submission task',
-                            tx_hash, receipt, self._signer.address,
-                        )
-                    else:
-                        self._logger.info(
-                            'tx_hash: {} succeeded for submission task', tx_hash,
-                        )
-                except Exception as e:
-                    self._logger.error(
-                        'tx_hash: {} failed to gather receipt after 120 seconds, error: {} | '
-                        'Context: Using signer {} for submission task',
-                        tx_hash, e, self._signer.address
-                    )
-                    self.pending_nonces.put(self._signer.nonce)
         finally:
             try:
-                self._signer.nonce_lock.writer_lock.release()
+                self._rwlock.writer_lock.release()
             except Exception as e:
-                logger.trace('Error releasing rwlock: {}. But moving on regardless... | Context: '
-                             'Using signer {} for submission task: {}. Acquiring lock', e, self._signer.address, kwargs)
+                logger.trace(
+                    'Error releasing rwlock: {}. But moving on regardless... | Context: '
+                    'Using signer {} for submission task: {}.', e, self._signer_address, kwargs,
+                )
     return wrapper
