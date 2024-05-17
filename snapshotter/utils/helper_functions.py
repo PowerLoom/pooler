69983a68b1c8a870c002b949d7a15eb13564f61c
anomit ghosh <anomit.ghosh@gmail.com>
2024-05-17 19:46:41 +0800

    fix: dont pile up stack on await context switches

--- snapshotter/utils/helper_functions.py
+++ snapshotter/utils/helper_functions.py
@@ -202,7 +202,8 @@ def aiorwlock_aqcuire_release(fn):
         # self._logger.debug('Wrapping fn: {}', fn.__name__)
         try:
             # including the retry calls
-            result = await fn(self, *args, **kwargs)
+            r =  await fn(self, *args, **kwargs)
+            return r
 
         except Exception as e:
             self._logger.opt(exception=True).error(
@@ -218,7 +219,6 @@ def aiorwlock_aqcuire_release(fn):
                     'Error releasing rwlock: {}. But moving on regardless... | Context: '
                     'Using signer {} for submission task: {}.', e, self._signer_address, kwargs,
                 )
-            return result
     return wrapper
 
 
