1b1f2313f2dfce0053286b0f6fa34d43444d4092
Seth <seth@powerloom.io>
2024-06-07 22:20:58 -0400

    chore: implement snapshotter identity check for devnet

--- snapshotter/snapshotter_id_ping.py
+++ snapshotter/snapshotter_id_ping.py
@@ -20,7 +20,46 @@ async def main():
     aioredis_pool = RedisPoolCache(pool_size=1000)
     await aioredis_pool.populate()
     redis_conn = aioredis_pool._aioredis_pool
-    
+
+    anchor_rpc = RpcHelper(settings.anchor_chain_rpc)
+    await anchor_rpc.init(redis_conn=redis_conn)
+    protocol_abi = read_json_file(settings.protocol_state.abi)
+    protocol_state_contract = anchor_rpc.get_current_node()['web3_client'].eth.contract(
+        address=Web3.toChecksumAddress(
+            settings.protocol_state.address,
+        ),
+        abi=protocol_abi,
+    )
+
+    snapshotter_address = to_checksum_address(settings.instance_id)
+
+    snapshotters_arr_query = await anchor_rpc.web3_call(
+        [
+            protocol_state_contract.functions.allSnapshotters(snapshotter_address),
+            protocol_state_contract.functions.slotSnapshotterMapping(settings.slot_id),
+        ],
+        redis_conn,
+    )
+
+    snapshotting_allowed = snapshotters_arr_query[0]
+    slot_id_address = snapshotters_arr_query[1]
+
+    if not snapshotting_allowed:
+        print('SIGNER_ACCOUNT_ADDRESS has not been assigned as a snapshotter. Exiting...')
+        await redis_conn.set(
+            active_status_key,
+            int(False),
+        )
+        sys.exit(1)
+    if to_checksum_address(slot_id_address) != to_checksum_address(settings.instance_id):
+        print('SIGNER_ACCOUNT_ADDRESS is not the one configured in SLOT_ID. Exiting...')
+        await redis_conn.set(
+            active_status_key,
+            int(False),
+        )
+        sys.exit(1)
+
+    print('Snapshotting allowed...')
     await redis_conn.set(
         active_status_key,
         int(True),
